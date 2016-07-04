fd9fe3e004916e2152f938702c2fa7b96d5bd0f7
char-lie <5962274+char-lie@users.noreply.github.com>
2016-07-04 10:04:39 +0300

    Fixed dot product C function

--- ctypes/basics.c
+++ ctypes/basics.c
@@ -6,8 +6,8 @@ int mul(int a, int b) {
 
 int dot(int *a, int *b, size_t length) {
     int result = 0;
-    do {
-        result += a[length-1] * b[length-1];
-    } while (--length > 0);
+    while (length --> 0) {
+        result += a[length] * b[length];
+    }
     return result;
 }
